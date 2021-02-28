0c87961ab784350f4dbdb9f9be44fe710ea6d296
kaisermelkon <kaisermelkon@gmail.com>
2021-02-28 12:18:39 -0400

    test: add header test

--- src/app/shared/header/header.component.spec.ts
+++ src/app/shared/header/header.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { HeaderComponent } from './header.component';
+
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +10,8 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent ],
+      imports: [TranslateModule.forRoot()]
     })
     .compileComponents();
   });
@@ -22,4 +25,16 @@ describe('HeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should change the value of language', () => {
+    spyOn(component, 'useLanguage').and.callThrough();
+    const e = fixture.debugElement.nativeElement.querySelectorAll('.dropdown-item');
+    e[1].click();
+    expect(component.useLanguage).toHaveBeenCalledWith('es');
+    expect(component.siteLanguage).toBe('Spanish');
+    e[0].click();
+    expect(component.useLanguage).toHaveBeenCalledWith('en');
+    expect(component.siteLanguage).toBe('English');
+  });
 });
+
