6d057edcd215e1c5c1618d3360ccfb2b33440aa8
kaisermelkon <kaisermelkon@gmail.com>
2021-03-01 17:28:43 -0400

    chore: add mocks

---
+++ src/app/shared/mocks/allProducts.mock.ts
@@ -0,0 +1,57 @@
+export const AllProductsMock: any =
+[
+    {
+        country: 'Colombia',
+        products: 
+        [
+            {
+                name: 'TShirt', 
+                price: 15, 
+                availability: true, 
+                quantitySold: 5,
+                category: 'Cloth'
+            },
+            {
+                name: 'Toaster', 
+                price: 23, 
+                availability: true, 
+                quantitySold: 7,
+                category: 'Home'
+            },
+            {
+                name: 'Iphone', 
+                price: 56, 
+                availability: false, 
+                quantitySold: 5,
+                category: 'Electronics'
+            }
+        ]
+    },
+    {
+        country: 'UnitedStatesOfAmerica', 
+        products: 
+        [
+            {
+                name: 'TShirt', 
+                price: 15, 
+                availability: true, 
+                quantitySold: 5,
+                category: 'Cloth'
+            }, 
+            {
+                name: 'Notebook', 
+                price: 23, 
+                availability: true, 
+                quantitySold: 7,
+                category: 'School'
+            },
+            {
+                name: 'Iphone', 
+                price: 56, 
+                availability: false, 
+                quantitySold: 5,
+                category: 'Electronics'
+            }
+        ]
+    }
+]
---
+++ src/app/shared/mocks/products.mock.ts
@@ -0,0 +1,24 @@
+export const ProductsMock: any =
+    [
+        {
+            name: 'TShirt',
+            price: 15, 
+            availability: true, 
+            quantitySold: 5,
+            category: 'Cloth'
+        }, 
+        {
+            name: 'Toaster', 
+            price: 23, 
+            availability: true, 
+            quantitySold: 7,
+            category: 'Home'
+        },
+        {
+            name: 'Iphone', 
+            price: 56, 
+            availability: false, 
+            quantitySold: 5,
+            category: 'Electronics'
+        }
+    ]
