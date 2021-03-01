da489cabc9f5dfaef992c2636dd6bc1699be537c
kaisermelkon <kaisermelkon@gmail.com>
2021-03-01 12:58:08 -0400

    test: add test for homepage filtering and sorting

--- src/app/homepage/homepage.component.spec.ts
+++ src/app/homepage/homepage.component.spec.ts
@@ -1,7 +1,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
-import { of } from 'rxjs/internal/observable/of';
 
 import { ProductService } from '../shared/services/product.service';
 
@@ -15,7 +15,7 @@ describe('HomepageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ HomepageComponent ],
-      imports: [TranslateModule.forRoot(), HttpClientTestingModule],
+      imports: [TranslateModule.forRoot(), HttpClientTestingModule, ReactiveFormsModule],
       providers: [ProductService]
     })
     .compileComponents();
@@ -34,23 +34,63 @@ describe('HomepageComponent', () => {
 
   it('should change category', () => {
     spyOn(component, 'changeCategory').and.callThrough();
+    let allProducts: any = [{country: 'Colombia', products: [{price: 15, availability: true, quantitySold: 5,category: 'Cloth'}, {price: 23, availability: true, quantitySold: 7,category: 'Home'},{price: 56, availability: false, quantitySold: 5,category: 'Electronics'}]},
+    {country: 'UnitedStatesOfAmerica', products: [{price: 15, availability: true, quantitySold: 5,category: 'Cloth'}, {price: 23, availability: true, quantitySold: 7,category: 'School'},{price: 56, availability: false, quantitySold: 5,category: 'Electronics'}]}
+    ]
+    let products: any =  [{price: 15, availability: true, quantitySold: 5,category: 'Cloth'}, {price: 23, availability: true, quantitySold: 7,category: 'Home'},{price: 56, availability: false, quantitySold: 5,category: 'Electronics'}]
     const e = fixture.debugElement.nativeElement.querySelectorAll('.nav-link');
     e[1].click();
-    expect(component.changeCategory).toHaveBeenCalledWith('Cloth');
-    const i = fixture.debugElement.nativeElement.querySelectorAll('.nav-link');
-    i[0].click();
-    expect(component.changeCategory).toHaveBeenCalledWith('Cloth');
+    expect(component.changeCategory).toHaveBeenCalled()
+    component.changeCategory('all', products, 'Colombia', allProducts)
+    expect(component.products).toEqual(products)
   });
 
-  it('should filter all by country', fakeAsync(() => {
-    productService.getProducts().subscribe(
-      (res) =>{
-        component.filterAllProductsByCountry('Colombia')
-        expect(component.filterAllProductsByCountry).toHaveBeenCalledWith('Colombia');
-        expect(component.filterAllProductsByCountry.length).toBe(12);
-      }
-    )
-    tick();
-  }));
+  it('should show availability', () => {
+    spyOn(component, 'showAvailability').and.callThrough();
+    const e = fixture.debugElement.nativeElement.querySelectorAll('.checkbox');
+    e[0].click();
+    expect(component.availability).toBe(true)
+  });
+
+  it('should change products by country', () => {
+    spyOn(component, 'filterAllProductsByCountry').and.callThrough();
+    let allProducts: any = [{country: 'Colombia', products: [{price: 15, availability: true, quantitySold: 5,category: 'Cloth'}, {price: 23, availability: true, quantitySold: 7,category: 'Home'},{price: 56, availability: false, quantitySold: 5,category: 'Electronics'}]},
+    {country: 'UnitedStatesOfAmerica', products: [{price: 15, availability: true, quantitySold: 5,category: 'Cloth'}, {price: 23, availability: true, quantitySold: 7,category: 'School'},{price: 56, availability: false, quantitySold: 5,category: 'Electronics'}]}
+    ]
+    let products: any =  [{price: 15, availability: true, quantitySold: 5,category: 'Cloth'}, {price: 23, availability: true, quantitySold: 7,category: 'Home'},{price: 56, availability: false, quantitySold: 5,category: 'Electronics'}]
+    let result = component.filterAllProductsByCountry(allProducts,  'Colombia')
+    expect(result).toEqual(products)
+  });
+
+  it('should change products by availability', () => {
+    spyOn(component, 'filterByAvailability').and.callThrough();
+    component.availability = true;
+    let products: any =  [{price: 15, availability: true, quantitySold: 5,category: 'Cloth'}, {price: 23, availability: true, quantitySold: 7,category: 'Home'},{price: 56, availability: false, quantitySold: 5,category: 'Electronics'}]
+    expect(component.filterByAvailability(products).length).toBe(2)
+    component.availability = false;
+    expect(component.filterByAvailability(products)).toEqual(products)
+  });
+
+  it('should change available', () => {
+    spyOn(component, 'changeAvailable').and.callThrough();
+    component.availability = true;
+    let allProducts: any = [{country: 'Colombia', products: [{price: 15, availability: true, quantitySold: 5,category: 'Cloth'}, {price: 23, availability: true, quantitySold: 7,category: 'Home'},{price: 56, availability: false, quantitySold: 5,category: 'Electronics'}]},
+    {country: 'UnitedStatesOfAmerica', products: [{price: 15, availability: true, quantitySold: 5,category: 'Cloth'}, {price: 23, availability: true, quantitySold: 7,category: 'School'},{price: 56, availability: false, quantitySold: 5,category: 'Electronics'}]}
+    ]
+    let products: any =  [{price: 15, availability: true, quantitySold: 5,category: 'Cloth'}, {price: 23, availability: true, quantitySold: 7,category: 'Home'},{price: 56, availability: false, quantitySold: 5,category: 'Electronics'}]
+    component.changeAvailable(products, 'Colombia', allProducts)
+    expect(component.availability).toBe(false)
+  });
+
+  it('should accept filter', () => {
+    spyOn(component, 'accept').and.callThrough();
+    let products: any =  [{price: 15, availability: true, quantitySold: 5,category: 'Cloth'}, {price: 23, availability: true, quantitySold: 7,category: 'Home'},{price: 56, availability: false, quantitySold: 5,category: 'Electronics'}]
+    expect(component.accept(products, 'Lowest Price')[0]).toEqual(products[0])
+    expect(component.accept(products, 'Highest Price')[0]).toEqual(products[0])
+    expect(component.accept(products, 'Best Selling')[0]).toEqual(products[0])
+
+  });
 
 });
+
+
