21e4ce16baabc93546e3b55c7093781621f88b5f
kaisermelkon <kaisermelkon@gmail.com>
2021-03-01 14:40:30 -0400

    feta: product page design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomepageComponent,
+  },
+  {
+    path: 'product/:name',
+    component: ProductPageComponent,
   },
 
 ];
