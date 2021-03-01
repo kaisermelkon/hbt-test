bd2626c2cf275de8d77274f59aa701dc732be812
kaisermelkon <kaisermelkon@gmail.com>
2021-03-01 03:52:32 -0400

    feat: add categories service

--- src/app/shared/services/categories.service.ts
+++ src/app/shared/services/categories.service.ts
@@ -3,16 +3,16 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { environment } from 'src/environments/environment';
 
+
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
+export class CategoriesService {
 
-  private baseUrl: string = `https://run.mocky.io/v3/`;
 
   constructor(private http: HttpClient) { }
 
-  public getProducts(): Observable<any> {
-    return this.http.get(`${environment.baseUrl}cb2bea6f-ed59-4329-8d83-b3ba5e7753e6`);
+  public getCategories(): Observable<any> {
+    return this.http.get(`${environment.baseUrl}23fa7d8a-4e31-4840-8e6f-f41444cfd361`);
   }
 }
