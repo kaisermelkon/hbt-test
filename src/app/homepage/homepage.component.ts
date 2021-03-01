347dedc14517f2adf95bd801d625c1130dfd2fe5
kaisermelkon <kaisermelkon@gmail.com>
2021-03-01 05:04:37 -0400

    feat: change products when country change

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,25 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from '../shared/services/categories.service';
 import { ProductService } from '../shared/services/product.service';
+import { ShareDataService } from '../shared/services/share-data.service';
+import { mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent implements OnInit {
+export class HomepageComponent implements OnInit{
 
   public products: Array<any> = []
 
+  private allProducts: Array<any> = []
+
   public categories: Array<string> = ['Cloth', 'House', 'Electronics'];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private categoriesService: CategoriesService, private shareData: ShareDataService) { }
 
   ngOnInit(): void {
-    this.productService.getProducts().subscribe(
+    this.productService.getProducts().pipe(
+      mergeMap((response: any) => {
+        this.allProducts = response.elements;
+        this.products = response.elements.filter((el: any)=> el.country === 'UnitedStatesOfAmerica')[0].products
+        return this.shareData.selectedCountry
+      }
+    )).subscribe((res3) => {
+      this.products = this.allProducts.filter((el: any)=> el.country === res3)[0].products
+    });
+    this.categoriesService.getCategories().subscribe(
       (response)=>{
         console.log(response)
-        this.products = response.elements;
-        console.log(this.products.length)
+        this.categories = response.elements.filter((el: any)=> el.country === 'UnitedStatesOfAmerica')[0].categories
       }
     )
   }
